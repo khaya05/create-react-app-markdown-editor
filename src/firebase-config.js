f698a68e4fd2821c51ca2c22e3c0947e6a33677a
khaya05 <khayamnyandu5@gmail.com>
2023-03-03 07:33:19 +0200

    added env vars

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVJTqKvqDq5Kagx1QCR6IQX68q_TbgcnM',
-  authDomain: 'markdown-editor-25439.firebaseapp.com',
-  projectId: 'markdown-editor-25439',
-  storageBucket: 'markdown-editor-25439.appspot.com',
-  messagingSenderId: '342480549126',
-  appId: '1:342480549126:web:99280c86ce0450e6c89a87',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
